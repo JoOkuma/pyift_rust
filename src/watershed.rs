3d14423e9082bf49661ad10390fa555e07faf963
Jordao Bragantini <you@example.com>
2023-09-24 20:26:44 -0700

    added macro for type, dim and pyfunction to watershed from minima

--- src/watershed.rs
+++ src/watershed.rs
@@ -1,19 +1,19 @@
-use std::ops::Add;
-use numpy::{Element};
 use ndarray::*;
+use numpy::{Element, IntoPyArray, PyArray, PyReadonlyArray};
+use pyo3::prelude::*;
+use std::ops::Add;
 
-use num_traits::{Bounded, Zero};
-use crate::heap::{ElemStatus, Heap};
 use crate::adjacency::{Adjacency, AdjacencyGrid2D, AdjacencyGrid3D};
-
+use crate::heap::{ElemStatus, Heap};
+use num_traits::{Bounded, Zero};
 
 pub fn watershed_from_minima<T, D>(
     topology: &ArrayView<T, D>,
     mask: &ArrayView<bool, D>,
     h: T,
 ) -> Array<usize, D>
-    where
-    T: Add<Output = T> + Bounded + Copy + Clone + Element + PartialOrd + Ord + Zero,
+where
+    T: Add<Output = T> + Bounded + Copy + Clone + Element + PartialOrd + Zero,
     D: Dimension,
 {
     let shape = topology.dim();
@@ -32,17 +32,11 @@ pub fn watershed_from_minima<T, D>(
     }
 
     let size: usize = topology.len();
-    let mut cost = topology
-        .to_shape(size)
-        .unwrap()
-        .mapv(|x| x + h);
+    let mut cost = topology.to_shape(size).unwrap().mapv(|x| x + h);
 
-    let mut root: Array1<usize> = Array1::from_shape_vec(
-        Ix1(size),
-        (0..size).collect()
-    ).unwrap();
+    let mut root: Array1<usize> = Array1::from_shape_vec(Ix1(size), (0..size).collect()).unwrap();
 
-    let mut heap = Heap::new(& mut cost);
+    let mut heap = Heap::new(&mut cost);
 
     for i in 0..size {
         if mask[i] {
@@ -50,22 +44,23 @@ pub fn watershed_from_minima<T, D>(
         }
     }
 
-    while !heap.is_empty()
-    {
+    while !heap.is_empty() {
         let p = heap.pop().unwrap();
 
-        if root[p] == p
-        {
+        if root[p] == p {
             heap.values[p] = topology[p];
         }
 
-        for q in adj.neighbors(p)
-        {
-            if mask[q] && heap.status[q] != ElemStatus::POPPED 
-            {
-                let path_cost = std::cmp::max(topology[q], heap.values[p]);
-                if path_cost < heap.values[q]
-                {
+        for q in adj.neighbors(p) {
+            if mask[q] && heap.status[q] != ElemStatus::POPPED {
+                let path_cost: T;
+                if topology[q] > heap.values[p] {
+                    path_cost = topology[q];
+                } else {
+                    path_cost = heap.values[p];
+                }
+
+                if path_cost < heap.values[q] {
                     root[q] = root[p];
                     heap.values[q] = path_cost;
                     heap.move_up(q, p as i64);
@@ -74,8 +69,7 @@ pub fn watershed_from_minima<T, D>(
         }
     }
 
-    for i in 0..size
-    {
+    for i in 0..size {
         if mask[i] {
             root[i] += 1;
         } else {
@@ -85,7 +79,6 @@ pub fn watershed_from_minima<T, D>(
 
     root.into_shape(shape).unwrap()
 }
-
 
 #[test]
 fn test_watershed_from_minima() {
@@ -97,10 +90,39 @@ fn test_watershed_from_minima() {
 
     let expected_labels = array![[1, 1, 1], [1, 1, 1], [1, 1, 1]];
 
-    let result = watershed_from_minima(
-        &image.view(),
-        &mask.view(),
-        1,
-    );
+    let result = watershed_from_minima(&image.view(), &mask.view(), 1);
     assert_eq!(result, expected_labels);
 }
+
+macro_rules! impl_watershed_from_minima {
+    ($new_name:ident, $ty:ty, $dim:expr) => {
+        // #[pyfunction]
+        pub fn $new_name<'py>(
+            py: Python<'py>,
+            topology: &PyReadonlyArray<$ty, Dim<[usize; $dim]>>,
+            mask: &PyReadonlyArray<bool, Dim<[usize; $dim]>>,
+            h: $ty,
+        ) -> PyResult<&'py PyArray<usize, Dim<[usize; $dim]>>> {
+            let arr = watershed_from_minima(&topology.as_array(), &mask.as_array(), h);
+            let py_array = arr.into_pyarray(py);
+            Ok(py_array)
+        }
+    };
+}
+
+impl_watershed_from_minima!(watershed_from_minima_u16_2d, u16, 2);
+impl_watershed_from_minima!(watershed_from_minima_u16_3d, u16, 3);
+impl_watershed_from_minima!(watershed_from_minima_u32_2d, u32, 2);
+impl_watershed_from_minima!(watershed_from_minima_u32_3d, u32, 3);
+
+impl_watershed_from_minima!(watershed_from_minima_i16_2d, i16, 2);
+impl_watershed_from_minima!(watershed_from_minima_i16_3d, i16, 3);
+impl_watershed_from_minima!(watershed_from_minima_i32_2d, i32, 2);
+impl_watershed_from_minima!(watershed_from_minima_i32_3d, i32, 3);
+impl_watershed_from_minima!(watershed_from_minima_i64_2d, i64, 2);
+impl_watershed_from_minima!(watershed_from_minima_i64_3d, i64, 3);
+
+impl_watershed_from_minima!(watershed_from_minima_f32_2d, f32, 2);
+impl_watershed_from_minima!(watershed_from_minima_f32_3d, f32, 3);
+impl_watershed_from_minima!(watershed_from_minima_f64_2d, f64, 2);
+impl_watershed_from_minima!(watershed_from_minima_f64_3d, f64, 3);
